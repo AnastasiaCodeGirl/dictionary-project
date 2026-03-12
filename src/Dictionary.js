0e5da9f6eaec86e13dfed8a0e9f18d44bb0606ed
AnastasiiaShumeiko <48533538+AnastasiaCodeGirl@users.noreply.github.com>
2026-03-12 13:33:32 +0100

    Results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
-  function handleResponse(response) {
+    let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
+    function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
   }
   function search(event) {
     event.preventDefault();
@@ -26,6 +30,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results}/>
     </div>
   );
 }
