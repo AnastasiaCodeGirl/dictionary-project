1df1f72415ed9f24dd44e5bd1e438a75160c3810
AnastasiiaShumeiko <48533538+AnastasiaCodeGirl@users.noreply.github.com>
2026-03-12 13:04:36 +0100

    improve Dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
-    function search(event){
-        event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
-    }
+  let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    console.log(apiUrl);
+    
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function handleKeywordChange(event){
-        console.log(event.target.value);
-        setKeyword(event.target.value);
-    }
+  function handleKeywordChange(event) {
+    console.log(event.target.value);
+    setKeyword(event.target.value);
+  }
 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
     </div>
   );
