a3fdd70cd607dadd52e411f20d3498fb91236133
AnastasiiaShumeiko <48533538+AnastasiaCodeGirl@users.noreply.github.com>
2026-03-12 12:07:23 +0100

    search Engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Dictionary from "./Dictionary";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <a href='#' className="btn btn-primary shadow">Hell no!</a>
+       <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"> 
+        <small>Coded by SheCodes</small>
+        </footer>
     </div>
   );
 }
