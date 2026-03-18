3e7fd88a0d508bd767aae9de81122aa5a94cb359
AnastasiiaShumeiko <48533538+AnastasiaCodeGirl@users.noreply.github.com>
2026-03-18 14:11:01 +0100

    Synonyms added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,27 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {/* {props.meaning.definitions.map(function (definition, index) {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             {definition.definition}
             <br />
             <em>{definition.example}</em>
+            <br/>
+            <Synonyms synonyms={definition.synonyms} />
+            {/* <strong>Symonyms:</strong>
+            {definition.synonyms} */}
           </div>
         );
-      })} */}
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p><em>{props.meaning.definitions[0].example}</em></p>
+      })}
+      {/* <p>{props.meaning.definitions[0].definition}</p>
+      <p><em>{props.meaning.definitions[0].example}</em></p> */}
+      
     </div>
   );
 }
