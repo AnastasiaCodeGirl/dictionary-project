fc5971d3bf2dc039de050b7de38b57710b9bac95
AnastasiiaShumeiko <48533538+AnastasiaCodeGirl@users.noreply.github.com>
2026-03-18 14:54:27 +0100

    Phonetics added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,21 +7,17 @@ export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
     function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
-    console.log(response.data[0].meanings[0].definitions[0].definition);
   }
   function search(event) {
     event.preventDefault();
     //API documentation link: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    console.log(apiUrl);
     
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
-    console.log(event.target.value);
     setKeyword(event.target.value);
   }
 
